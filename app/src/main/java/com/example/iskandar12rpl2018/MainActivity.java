6cd978777062f232a86711aac16a529150ade720
muhamadarzakhidayatullah123 <mmarzak905@gmail.com>
2020-07-24 10:27:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/iskandar12rpl2018/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iskandar12rpl2018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
